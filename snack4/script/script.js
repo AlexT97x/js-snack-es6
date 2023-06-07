b4ef7d21b64d3a977edf118d25cc6b0d42bf8147
AlexT97x <alextinella@gmail.com>
2023-06-07 19:51:09 +0200

    snack 4, creazione array oggetti

---
+++ snack4/script/script.js
@@ -0,0 +1,105 @@
+const squadreSerieA = [
+    {
+      nome: 'Napoli',
+      punti_fatti: 0,
+      falli_subiti: 0,
+    },
+    {
+      nome: 'Lazio',
+      punti_fatti: 0,
+      falli_subiti: 0,
+    },
+    {
+      nome: 'Inter',
+      punti_fatti: 0,
+      falli_subiti: 0,
+    },
+    {
+      nome: 'Milan',
+      punti_fatti: 0,
+      falli_subiti: 0,
+    },
+    {
+      nome: 'Atalanta',
+      punti_fatti: 0,
+      falli_subiti: 0,
+    },
+    {
+      nome: 'Roma',
+      punti_fatti: 0,
+      falli_subiti: 0,
+    },
+    {
+      nome: 'Juventus',
+      punti_fatti: 0,
+      falli_subiti: 0,
+    },
+    {
+      nome: 'Fiorentina',
+      punti_fatti: 0,
+      falli_subiti: 0,
+    },
+    {
+      nome: 'Bologna',
+      punti_fatti: 0,
+      falli_subiti: 0,
+    },
+    {
+      nome: 'Torino',
+      punti_fatti: 0,
+      falli_subiti: 0,
+    },
+    {
+      nome: 'Monza',
+      punti_fatti: 0,
+      falli_subiti: 0,
+    },
+    {
+      nome: 'Udinese',
+      punti_fatti: 0,
+      falli_subiti: 0,
+    },
+    {
+      nome: 'Sassuolo',
+      punti_fatti: 0,
+      falli_subiti: 0,
+    },
+    {
+      nome: 'Empoli',
+      punti_fatti: 0,
+      falli_subiti: 0,
+    },
+    {
+      nome: 'Salernitana',
+      punti_fatti: 0,
+      falli_subiti: 0,
+    },
+    {
+      nome: 'Lecce',
+      punti_fatti: 0,
+      falli_subiti: 0,
+    },
+    {
+      nome: 'Spezia',
+      punti_fatti: 0,
+      falli_subiti: 0,
+    },
+    {
+      nome: 'Verona',
+      punti_fatti: 0,
+      falli_subiti: 0,
+    },
+    {
+      nome: 'Cremonese',
+      punti_fatti: 0,
+      falli_subiti: 0,
+    },
+    {
+      nome: 'Sampdoria',
+      punti_fatti: 0,
+      falli_subiti: 0,
+    },
+  ];
+  
+  console.log(squadreSerieA);
+  
